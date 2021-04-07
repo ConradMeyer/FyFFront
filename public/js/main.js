2eaa23e7a1151db778f290377d8b18e22d87c566
ConradMeyer <meyeralten@gmail.com>
2021-04-07 18:32:23 +0200

    scraper almost done

--- public/js/main.js
+++ public/js/main.js
@@ -14,13 +14,25 @@ function search() {
 
     fetch("/search", options)
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => res.map(el => pintar(el)))
       .catch(err => console.log(err))
 }
 
 function pintar(data) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "oferta")
 
+  let h2 = document.createElement("h2")
+  let title = document.createTextNode(data.titulo)
+  h2.appendChild(title)
+  div.appendChild(h2)
 
+  let text = document.createElement("h3")
+  let resm = document.createTextNode(data.resumen)
+  text.appendChild(resm)
+  div.appendChild(text)
+
+  RESULT.appendChild(div)
 }
 
 BUSCAR.addEventListener("click", () => search());
